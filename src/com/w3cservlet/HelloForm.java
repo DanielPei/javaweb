88f7096f1e709111cd13452c7b4dcef262f969ac
DanielPei <peixq1222@icloud.com>
2015-11-11 21:03:17 +0800

    2015.11.10-summary-for-servlet

---
+++ src/com/w3cservlet/HelloForm.java
@@ -0,0 +1,68 @@
+package com.w3cservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HellowForm
+ */
+
+//	http://localhost/javaweb/HellowForm?first_name=ZARA&last_name=ALI
+@WebServlet("/HelloForm")
+public class HelloForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//防止中文乱码
+		request.setCharacterEncoding("GBK");
+		response.setCharacterEncoding("GBK");
+		
+	      // 设置响应内容类型
+	      response.setContentType("text/html");
+
+	      PrintWriter out = response.getWriter();
+		  String title = "Using GET Method to Read Form Data";
+	      String docType =
+	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+	      "transitional//en\">\n";
+	      out.println(docType +
+	                "<html>\n" +
+	                "<head><title>" + title + "</title></head>\n" +
+	                "<body bgcolor=\"#f0f0f0\">\n" +
+	                "<h1 align=\"center\">" + title + "</h1>\n" +
+	                "<ul>\n" +
+	                "  <li><b>名字</b>："
+	                + request.getParameter("first_name") + "\n" +
+	                "  <li><b>姓氏</b>："
+	                + request.getParameter("last_name") + "\n" +
+	                "</ul>\n" +
+	                "</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/w3cservlet/HelloWorld.java
@@ -0,0 +1,56 @@
+package com.w3cservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+//@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String message;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	// TODO Auto-generated method stub
+    	super.init();
+        // 执行必需的初始化
+        message = "Hello World";
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  // 设置响应内容类型
+		  response.setContentType("text/html");
+		
+		  // 实际的逻辑是在这里
+		  PrintWriter out = response.getWriter();
+		  out.println("<h1>" + message + "</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
